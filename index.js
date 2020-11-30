eeeefd9f699695420d2ce3023c83771c887a828b
Tanaji Deshmukh <tanaji.ashokdeshmukh@squarepixelstudios.net>
2020-11-30 16:30:54 +0530

    folow link changes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyparser.json());
 passport.use(new Strategy({
     consumerKey:process.env.twitter_consumer_key,
     consumerSecret:process.env.twitter_sceret_key,
-    callbackURL:"http://localhost:3000/app/twitter/return"
+    callbackURL:"http://folow-backend.inkincaps.com//app/twitter/return"
 },function(token,tokenSceret,profile,callback){
     callback(null,profile);
 }));
